9e84772c5a267217f0921a712aa9c289317075cd
erikgunawan <erik.gunawan44@gmail.com>
2023-03-17 09:46:48 +0700

    adjust repository

--- app/src/main/java/id/gliacademy/sampleproject/api_arch/repository/ProvinceRepository.kt
+++ app/src/main/java/id/gliacademy/sampleproject/api_arch/repository/ProvinceRepository.kt
@@ -1,57 +1,15 @@
 package id.gliacademy.sampleproject.api_arch.repository
 
-import android.util.Log
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import id.gliacademy.sampleproject.api_arch.data.services.ApiConfig
 import id.gliacademy.sampleproject.api_arch.data.services.ProvinceResponse
 import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class ProvinceRepository {
 
   val model = MutableLiveData<ProvinceRepositoryModel>()
 
-  fun getProvinces(): LiveData<ProvinceRepositoryModel> {
-    model.value = ProvinceRepositoryModel(
-      isLoading = true,
-      state = ProvinceRepositoryModel.STATE_LOADING
-    )
-
-    val client = ApiConfig.getApiService().getProvince()
-
-    client.enqueue(object : Callback<ProvinceResponse> {
-      override fun onResponse(
-        call: Call<ProvinceResponse>,
-        response: Response<ProvinceResponse>
-      ) {
-        model.value = ProvinceRepositoryModel(
-          isLoading = false
-        )
-
-        if (response.isSuccessful) {
-          model.value = ProvinceRepositoryModel(
-            provinces = response.body()?.province ?: arrayListOf(),
-            isLoading = false
-          )
-        } else {
-          model.value = ProvinceRepositoryModel(
-            provinces = response.body()?.province ?: arrayListOf(),
-            isLoading = false,
-            error = response.message()
-          )
-          Log.e("TAG", "onFailure: ${response.message()}")
-        }
-      }
-
-      override fun onFailure(call: Call<ProvinceResponse>, t: Throwable) {
-        model.value = ProvinceRepositoryModel(
-          isLoading = false
-        )
-        Log.e("TAG", "onFailure: ${t.message.toString()}")
-      }
-    })
-    return model
+  fun getProvinces(): Call<ProvinceResponse> {
+    return ApiConfig.getApiService().getProvince()
   }
 }
--- app/src/main/java/id/gliacademy/sampleproject/api_arch/view/ApiViewModel.kt
+++ app/src/main/java/id/gliacademy/sampleproject/api_arch/view/ApiViewModel.kt
@@ -1,10 +1,15 @@
 package id.gliacademy.sampleproject.api_arch.view
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import id.gliacademy.sampleproject.api_arch.data.services.ProvinceResponse
 import id.gliacademy.sampleproject.api_arch.repository.ProvinceRepository
 import id.gliacademy.sampleproject.api_arch.repository.ProvinceRepositoryModel
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ApiViewModel : ViewModel() {
 
@@ -20,6 +25,36 @@ class ApiViewModel : ViewModel() {
   fun getProvinceRepositoryModel(): LiveData<ProvinceRepositoryModel> = _provinceRepositoryModel
 
   private fun getProvinces() {
-    _provinceRepositoryModel = repository.getProvinces() as MutableLiveData<ProvinceRepositoryModel>
+    repository.getProvinces().enqueue(object : Callback<ProvinceResponse> {
+    override fun onResponse(
+      call: Call<ProvinceResponse>,
+      response: Response<ProvinceResponse>
+    ) {
+      _provinceRepositoryModel.value = ProvinceRepositoryModel(
+        isLoading = false
+      )
+
+      if (response.isSuccessful) {
+        _provinceRepositoryModel.value = ProvinceRepositoryModel(
+          provinces = response.body()?.province ?: arrayListOf(),
+          isLoading = false
+        )
+      } else {
+        _provinceRepositoryModel.value = ProvinceRepositoryModel(
+          provinces = response.body()?.province ?: arrayListOf(),
+          isLoading = false,
+          error = response.message()
+        )
+        Log.e("TAG", "onFailure: ${response.message()}")
+      }
+    }
+
+    override fun onFailure(call: Call<ProvinceResponse>, t: Throwable) {
+      _provinceRepositoryModel.value = ProvinceRepositoryModel(
+        isLoading = false
+      )
+      Log.e("TAG", "onFailure: ${t.message.toString()}")
+    }
+  })
   }
 }
